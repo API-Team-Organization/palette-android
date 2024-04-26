b5cca817b1544ad5ce634bbeb535cf3ae8edaa61
seo1016 <seoseunghun3@gmail.com>
2024-04-26 10:30:20 +0900

    feat :: Add maxDate, defaultDate to JoinBirthFragment

--- app/src/main/java/com/example/palette/ui/signup/JoinBirthFragment.kt
+++ app/src/main/java/com/example/palette/ui/signup/JoinBirthFragment.kt
@@ -1,14 +1,18 @@
 package com.example.palette.ui.signup
 
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
+import android.widget.NumberPicker
 import androidx.navigation.fragment.findNavController
 import com.example.palette.R
 import com.example.palette.databinding.FragmentJoinBirthBinding
 import com.example.palette.databinding.FragmentJoinPasswordBinding
+import java.util.Calendar
 
 class JoinBirthFragment : Fragment() {
     private lateinit var binding : FragmentJoinBirthBinding
@@ -19,6 +23,20 @@ class JoinBirthFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentJoinBirthBinding.inflate(inflater, container, false)
+
+        val datePicker = binding.dpSpinner
+        datePicker.maxDate = System.currentTimeMillis() - 1000
+
+        val calendar = Calendar.getInstance()
+        calendar.set(2000, Calendar.JANUARY, 1)
+
+        datePicker.init(
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        ) { view, year, monthOfYear, dayOfMonth ->
+            // 날짜가 변경될 때 실행할 작업
+        }
 
         binding.btnContinue.setOnClickListener {
             findNavController().navigate(R.id.action_joinBirthFragment_to_joinNameFragment)
