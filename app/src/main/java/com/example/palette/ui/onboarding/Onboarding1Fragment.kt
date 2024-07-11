5a6d6bd004d78ad5391362417e1485786b2958e1
seo1016 <seoseunghun3@gmail.com>
2024-07-11 22:22:00 +0900

    Fix :: 온보딩 수정

--- app/src/main/java/com/example/palette/ui/onboarding/Onboarding1Fragment.kt
+++ app/src/main/java/com/example/palette/ui/onboarding/Onboarding1Fragment.kt
@@ -1,21 +1,32 @@
 package com.example.palette.ui.onboarding
 
+import android.media.Image
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.DrawableImageViewTarget
 import com.example.palette.R
+import com.example.palette.databinding.FragmentOnboarding1Binding
 
 class Onboarding1Fragment : Fragment() {
+
+    private lateinit var binding: FragmentOnboarding1Binding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        binding = FragmentOnboarding1Binding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
 
+        val onboarding1: ImageView = binding.onBoarding1Animation
+        val gifImage = DrawableImageViewTarget(onboarding1)
+        Glide.with(this).load(R.drawable.onboarding1).into(gifImage)
 
-        return inflater.inflate(R.layout.fragment_onboarding1, container, false)
+        return binding.root
     }
 
 }
--- app/src/main/java/com/example/palette/ui/onboarding/Onboarding3Fragment.kt
+++ app/src/main/java/com/example/palette/ui/onboarding/Onboarding3Fragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.DrawableImageViewTarget
 import com.example.palette.R
 import com.example.palette.databinding.FragmentOnboarding3Binding
 
@@ -16,6 +19,11 @@ class Onboarding3Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentOnboarding3Binding.inflate(inflater, container, false)
+
+        val onboarding3: ImageView = binding.onBoarding3Animation
+        val gifImage = DrawableImageViewTarget(onboarding3)
+        Glide.with(this).load(R.drawable.onboarding3).into(gifImage)
+
         binding.onBoarding3Button.setOnClickListener {
             findNavController().navigate(R.id.action_onboardingDefaultFragment_to_startFragment)
         }
