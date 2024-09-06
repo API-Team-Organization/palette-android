77b13b08ade51dfc840642790fe65f92d3761a99
qpeterp <lsepeter0827@naver.com>
2024-09-06 20:52:00 +0900

    Refactor :: BaseURL & okHttpClient merge

--- app/src/main/java/com/example/palette/data/info/InfoRequestManager.kt
+++ app/src/main/java/com/example/palette/data/info/InfoRequestManager.kt
@@ -2,18 +2,13 @@ package com.example.palette.data.info
 
 import android.util.Log
 import com.example.palette.common.Constant
+import com.example.palette.data.ApiClient
 import com.example.palette.data.base.BaseResponse
 import com.example.palette.data.base.BaseVoidResponse
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 object InfoRequestManager {
-    private val retrofit = Retrofit.Builder()
-        .baseUrl("https://paletteapp.xyz/backend/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-    private val infoService: InfoService = retrofit.create(InfoService::class.java)
+    private val infoService: InfoService = ApiClient.retrofit.create(InfoService::class.java)
 
     suspend fun profileInfoRequest(token: String): BaseResponse<ProfileData>? {
         val response = infoService.profileInfo(token, "*/*")
