b976d1f2c77e1a4f617d8680d28aa83ca4483b73
seo1016 <seoseunghun3@gmail.com>
2024-09-09 21:55:40 +0900

    Feat :: MyInfo view

---
+++ app/src/main/java/com/example/palette/ui/main/settings/MyInfoFragment.kt
@@ -0,0 +1,22 @@
+package com.example.palette.ui.main.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.palette.databinding.FragmentMyInfoBinding
+
+class MyInfoFragment : Fragment() {
+
+    private lateinit var binding: FragmentMyInfoBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMyInfoBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
