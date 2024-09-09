2d3fee9beea4f03002dae6baad0be1411d9d22b3
seo1016 <seoseunghun3@gmail.com>
2024-09-09 23:13:01 +0900

    Feat :: SettingFragment: load username info

--- app/src/main/java/com/example/palette/ui/main/settings/SettingFragment.kt
+++ app/src/main/java/com/example/palette/ui/main/settings/SettingFragment.kt
@@ -1,12 +1,19 @@
 package com.example.palette.ui.main.settings
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.example.palette.R
+import com.example.palette.application.PaletteApplication
+import com.example.palette.application.UserPrefs
+import com.example.palette.common.Constant
+import com.example.palette.data.info.InfoRequestManager
 import com.example.palette.databinding.FragmentSettingBinding
+import kotlinx.coroutines.launch
 
 class SettingFragment : Fragment() {
 
@@ -18,6 +25,8 @@ class SettingFragment : Fragment() {
     ): View {
         binding = FragmentSettingBinding.inflate(inflater, container, false)
 
+        loadUserNameInfo()
+
         binding.llMy.setOnClickListener {
             requireActivity().supportFragmentManager.beginTransaction()
                 .replace(R.id.mainContent, MyInfoFragment())
@@ -26,5 +35,21 @@ class SettingFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    private fun loadUserNameInfo() {
+        binding.tvUserName.text = UserPrefs.userName
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            try {
+                val profileInfo = InfoRequestManager.profileInfoRequest(PaletteApplication.prefs.token)
+                profileInfo?.data?.let { data ->
+                    binding.tvUserName.text = data.name
+                    UserPrefs.userName = data.name
+                }
+            } catch (e: Exception) {
+                Log.e(Constant.TAG, "Setting UserNameInfo error : ", e)
+            }
+        }
     }
 }
