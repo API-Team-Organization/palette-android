959e674068adb6fe9780a1638f3e1d7376e9022c
qpeterp <lsepeter0827@naver.com>
2024-09-10 20:36:33 +0900

    Feat :: Add LoggerUtils log e, log i

--- app/src/main/java/com/example/palette/ui/util/LoggerUtils.kt
+++ app/src/main/java/com/example/palette/ui/util/LoggerUtils.kt
@@ -6,3 +6,11 @@ import com.example.palette.common.Constant
 fun log(message: String) {
     Log.d(Constant.TAG, message)
 }
+
+fun logE(message: String) {
+    Log.e(Constant.TAG, message)
+}
+
+fun logI(message: String) {
+    Log.i(Constant.TAG, message)
+}
