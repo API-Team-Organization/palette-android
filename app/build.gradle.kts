2f212850ef45f906d057d06de389c318ade571e7
seo1016 <seoseunghun3@gmail.com>
2024-10-14 09:46:37 +0900

    Feat :: SELECTABLE, GRID

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,4 +104,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.1")
 }
