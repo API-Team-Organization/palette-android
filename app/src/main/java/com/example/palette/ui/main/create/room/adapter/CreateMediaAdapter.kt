b3df43573204c7c1a8ed20b20b2bb044a25d27c9
qpeterp <lsepeter0827@naver.com>
2024-10-16 08:17:51 +0900

    Fix :: Room last message can click [ 마지막 메세지 띄워둔 거도 누를 수 있게 변경 ]

--- app/src/main/java/com/example/palette/ui/main/create/room/adapter/CreateMediaAdapter.kt
+++ app/src/main/java/com/example/palette/ui/main/create/room/adapter/CreateMediaAdapter.kt
@@ -1,8 +1,12 @@
 package com.example.palette.ui.main.create.room.adapter
 
+import android.annotation.SuppressLint
+import android.text.Spannable
 import android.text.TextUtils
 import android.text.method.LinkMovementMethod
+import android.text.method.MovementMethod
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -21,10 +25,27 @@ class CreateMediaAdapter(
         return WorkViewHolder(view)
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onBindViewHolder(holder: WorkViewHolder, position: Int) {
         holder.iv_logo.setImageResource(R.drawable.logo)
         holder.tv_poster_title.text = itemList[position].title
         holder.tv_poster_desc.text = lastMessageList[position]
+
+        // 터치 이벤트를 통해 클릭 처리
+        holder.tv_poster_desc.setOnTouchListener { v, event ->
+            val textView = v as TextView
+            val movementMethod: MovementMethod = textView.movementMethod
+
+            // 링크 터치 이벤트 처리
+            val touchHandled = movementMethod.onTouchEvent(textView, textView.text as Spannable, event)
+
+            if (!touchHandled && event.action == MotionEvent.ACTION_UP) {
+                // 터치가 링크가 아닐 때 클릭 처리
+                itemClickListener.onItemClick(holder.bindingAdapterPosition, holder.itemView)
+            }
+
+            touchHandled
+        }
 
         holder.tv_poster_desc.movementMethod = LinkMovementMethod.getInstance()
         holder.tv_poster_desc.ellipsize = TextUtils.TruncateAt.MARQUEE
@@ -35,7 +56,7 @@ class CreateMediaAdapter(
     override fun getItemCount(): Int = itemList.size
 
     interface OnItemClickListener {
-        fun onItemClick(position: Int)
+        fun onItemClick(position: Int, itemView: View)
         fun onItemLongClick(position: Int)
     }
 
@@ -48,7 +69,7 @@ class CreateMediaAdapter(
 
         init {
             itemView.setOnClickListener {
-                itemClickListener.onItemClick(bindingAdapterPosition)
+                itemClickListener.onItemClick(bindingAdapterPosition, itemView)
             }
 
             itemView.setOnLongClickListener {
