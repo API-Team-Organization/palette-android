5a6a1fe9249b9ed29d75f00eb4308294f167d5b7
seo1016 <seoseunghun3@gmail.com>
2024-10-16 14:09:44 +0900

    Feat :: Rive 애니메이션 적용

--- app/src/main/java/com/example/palette/ui/onboarding/Onboarding1Fragment.kt
+++ app/src/main/java/com/example/palette/ui/onboarding/Onboarding1Fragment.kt
@@ -1,14 +1,11 @@
 package com.example.palette.ui.onboarding
 
-import android.media.Image
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.target.DrawableImageViewTarget
+import app.rive.runtime.kotlin.RiveAnimationView
 import com.example.palette.R
 import com.example.palette.databinding.FragmentOnboarding1Binding
 
@@ -20,11 +17,8 @@ class Onboarding1Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentOnboarding1Binding.inflate(inflater, container, false)
-        // Inflate the layout for this fragment
-
-        val onboarding1: ImageView = binding.onBoarding1Animation
-        val gifImage = DrawableImageViewTarget(onboarding1)
-        Glide.with(this).load(R.drawable.onboarding1).into(gifImage)
+        val riveAnimationView: RiveAnimationView = binding.onBoarding1Animation
+        riveAnimationView.setRiveResource(R.raw.message_icon_new)
 
         return binding.root
     }
