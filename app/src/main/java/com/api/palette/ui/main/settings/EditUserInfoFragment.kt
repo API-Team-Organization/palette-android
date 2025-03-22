a104d1276d620214d4539467acb5e7019bac3e68
seo1016 <seoseunghun3@gmail.com>
2025-03-22 17:43:30 +0900

    refactoring

--- app/src/main/java/com/api/palette/ui/main/settings/EditUserInfoFragment.kt
+++ app/src/main/java/com/api/palette/ui/main/settings/EditUserInfoFragment.kt
@@ -1,44 +1,43 @@
 package com.api.palette.ui.main.settings
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.api.palette.R
+import androidx.fragment.app.Fragment
 import com.api.palette.databinding.FragmentEditUserInfoBinding
-import com.api.palette.ui.base.BaseControllable
-import com.api.palette.ui.main.ServiceActivity
 import com.api.palette.ui.util.changeFragment
 
 class EditUserInfoFragment : Fragment() {
 
-    private lateinit var binding: FragmentEditUserInfoBinding
+    private var _binding: FragmentEditUserInfoBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentEditUserInfoBinding.inflate(inflater, container, false)
+        _binding = FragmentEditUserInfoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-        (requireActivity() as? BaseControllable)?.bottomVisible(false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         binding.changePassword.setOnClickListener {
             changeFragment(ChangePasswordFragment())
         }
-
         binding.changeName.setOnClickListener {
             changeFragment(ChangeNameFragment())
         }
-
         binding.changeBirthDate.setOnClickListener {
             changeFragment(ChangeBirthDateFragment())
         }
-
-        return binding.root
     }
 
-    override fun onPause() {
-        super.onPause()
-        (activity as ServiceActivity).findViewById<View>(R.id.bottomBar).visibility = View.VISIBLE
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
